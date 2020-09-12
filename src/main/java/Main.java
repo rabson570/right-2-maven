3915c51cf0477b5d74ed83b6583c5e832c8e1e0e
rabson570 <rabson570@gmail.com>
2020-09-12 18:42:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the size of triangle side: ");
+        int a = input.nextInt();
+        for (int i=1;i<=a;i++){{
+            for (int j=(a-i);j>=1;j--)
+                System.out.print(" ");
+            for (int k=1;k<=i;k++)
+                System.out.print("*");
+        }
+            System.out.println();
+        }
+    }
+
+}
